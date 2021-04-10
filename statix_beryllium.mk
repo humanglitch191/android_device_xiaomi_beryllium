6c64672c1e8fbf2757e32f398de6d3924f785804
humanglitch191 <humanglitch1.1@gmail.com>
2021-04-10 03:37:29 +0000

    beryllium: Adapt for StatiXOS
    Change-Id: I290cf5e1b246d81e09dd52a7a83041a36b589e05

--- statix_beryllium.mk
+++ statix_beryllium.mk
@@ -6,11 +6,12 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common StatiXOS stuff.
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := statix_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
